8765210691d62a3cbcb4341641b4ceb6b58814a2
cainvampyr <cainvampyr@gmail.com>
2015-08-09 20:41:17 +0100

    Implemented the actual observer pattern on the observer decorator

--- module/darkhounds/decorator/strictArguments.js
+++ module/darkhounds/decorator/strictArguments.js
@@ -4,9 +4,6 @@ var _constructor    = decorator.extend(function(){
     throw new Error(_constructor.ERROR_CANNOT_INSTANTIATE_DECORATOR_CLASS);
 });
 Object.defineProperty(_constructor, 'toString', {value: function(){ return 'Decorator StrictArguments' }});
-Object.defineProperty(_constructor, 'ERROR_CANNOT_INSTANTIATE_DECORATOR_CLASS',
-        {enumerable: true, value: 'Cannot instantiate decorator class'});
-
 Object.defineProperties(_constructor, {
     ERROR_MISSING_ARGUMENT: {enumerable:true, value:"Missing argument"},
     ERROR_WRONG_ARGUMENT:   {enumerable:true, value:"Wrong argument type"}
--- module/darkhounds/test/decoratorStrictArguments.coffee
+++ module/darkhounds/test/decoratorStrictArguments.coffee
@@ -6,4 +6,5 @@ describe 'The Decorator StrictArguments', ->
     it 'Should implement the "decorate" method',        -> StrictArguments.should.itself.respondTo 'decorate'
     it 'Should have missing argument error declared',   -> StrictArguments.ERROR_MISSING_ARGUMENT.should.be.a 'string'
     it 'Should have wrong argument type declared',      -> StrictArguments.ERROR_WRONG_ARGUMENT.should.be.a 'string'
-    
+    it 'Should throw an error when trying to instantiate',  ->
+        StrictArguments.bind(undefined).should.throw Error, StrictArguments.ERROR_CANNOT_INSTANTIATE_DECORATOR_CLASS
