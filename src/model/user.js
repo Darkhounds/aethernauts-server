9a1faea4d758131c04a2dbd7b9bfd94c547bdca7
cainvampyr <cainvampyr@gmail.com>
2015-08-15 14:40:09 +0100

    Coping with the changes done to the darkhounds framework

--- src/model/user.js
+++ src/model/user.js
@@ -1,20 +1,11 @@
-model               = require("darkhounds/src/abstract/model.js");
+ Model               = require("darkhounds").abstract.Model;
 
-var _constructor    =function(driver)                                                                     {
-    model.decorate(this);
-    this._driverChanged = function(){};
-    this.setDriver(driver)
+var constructor    =function(driver)                                                                     {
+    Model(this);
     
-//    /*Abstract Model*/
-//    if (!driver) throw new Error(_constructor.ERROR_MISSING_ARGUMENT);
-//    if (typeof driver !== 'object') throw new Error(_constructor.ERROR_WRONG_ARGUMENT);
-//    //
-//    var _driver = driver;
-//    //
-//    observable.decorate(this);
-//    this.getDriver = function() { return _driver; };
-//    //
-//    return this;
+    this.setDriver(driver);
+    
+    return this;
 };
-Object.defineProperty(_constructor, 'toString', {value: function(){ return 'Model User'; }});
-module.exports      = _constructor;
+
+module.exports      = constructor;
